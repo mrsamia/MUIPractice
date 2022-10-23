dbb30374d333f13c199c5e484077970fe259596a
mrsamia <15203060@iubat.edu>
2022-10-23 21:14:48 +0600

    SRM | FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import TextField from '@mui/material/TextField';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import {  Tooltip, Typography } from '@mui/material';
+
+const sx = {
+  height: 80,
+  width: 180
+}
 
 function App() {
+
+  function ClickHandler() {
+    alert("click me")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='pt-4'>
+        <Typography variant='h4'>This is My First MUI Project</Typography>
+      </div>
+      <div className='pt-4'>
+        <Tooltip arrow placement='right' title="This is Mui button">
+          <Button variant="contained" color='success' size="large" sx={sx} startIcon={<AccessAlarmIcon />}> React App </Button>
+        </Tooltip>
+      </div>
+      <div className='pt-4'>
+        <Button variant="contained" color='success' size="large" classes={{ root: "bttn" }} endIcon={<AccountBalanceIcon />} onClick={ClickHandler}> React App </Button><br></br>
+      </div>
+      <div className='pt-4'>
+        <ButtonGroup variant="contained" aria-label="outlined primary button group">
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
+      </div>
+      <div className='pt-4'>
+        <TextField label="Filled" />
+      </div>
+    
+
     </div>
   );
 }
